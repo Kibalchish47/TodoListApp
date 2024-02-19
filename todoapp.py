4270d4d540cf2bcc5a5f252a8aa4b95b56bf1821
Kibalchish47 <kibalchishthecoder@gmail.com>
2024-02-19 18:40:22 +0200

    Imports

---
+++ todoapp.py
@@ -0,0 +1,5 @@
+# importing the required modules  
+import tkinter as tk                    # importing the tkinter module as tk  
+from tkinter import ttk                 # importing the ttk module from the tkinter library  
+from tkinter import messagebox          # importing the messagebox module from the tkinter library  
+import sqlite3 as sql                   # importing the sqlite3 module as sql  
