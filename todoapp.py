0ac7112c21ebb5982cd4a93fe481d3c778bef173
Kibalchish47 <kibalchishthecoder@gmail.com>
2024-02-19 18:41:19 +0200

    add_tasks function

--- todoapp.py
+++ todoapp.py
@@ -3,3 +3,22 @@ import tkinter as tk                    # importing the tkinter module as tk
 from tkinter import ttk                 # importing the ttk module from the tkinter library  
 from tkinter import messagebox          # importing the messagebox module from the tkinter library  
 import sqlite3 as sql                   # importing the sqlite3 module as sql  
+
+  
+# defining the function to add tasks to the list  
+def add_task():  
+    # getting the string from the entry field  
+    task_string = task_field.get()  
+    # checking whether the string is empty or not  
+    if len(task_string) == 0:  
+        # displaying a message box with 'Empty Field' message  
+        messagebox.showinfo('Error', 'Field is Empty.')  
+    else:  
+        # adding the string to the tasks list  
+        tasks.append(task_string)  
+        # using the execute() method to execute a SQL statement  
+        the_cursor.execute('insert into tasks values (?)', (task_string ,))  
+        # calling the function to update the list  
+        list_update()  
+        # deleting the entry in the entry field  
+        task_field.delete(0, 'end')  
